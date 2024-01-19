0f17d293a28274700fe7f17a8d7b918a13f784f9
djuanp <juandomenech2003@gmail.com>
2024-01-19 18:25:35 +0100

    Add files via upload

--- resolvers/query.ts
+++ resolvers/query.ts
@@ -11,25 +11,7 @@ export const Query = {
                 throw new GraphQLError("Contacto no encontrado");
             }
 
-            const urlWorldTime = "https://api.api-ninjas.com/v1/worldtime?city="+contactExist.capital;
-            const response3 = await fetch(urlWorldTime, {
-                headers: {
-                    'X-Api-Key': NINJA_KEY as string
-                }
-            })
-            const json3 = await response3.json();
-            const datetimeNew = json3.datetime;
-
-            const contactUpdate = await ContactModel.findByIdAndUpdate(
-                {_id: args.id},
-                {datetime: datetimeNew},
-                {new: true}
-            ).exec();
-            if(!contactUpdate){
-                throw new GraphQLError("Error");
-            }
-            await contactUpdate.save();
-            return contactUpdate;
+            return contactExist;
 
         }catch(e){
             throw new GraphQLError(e);
